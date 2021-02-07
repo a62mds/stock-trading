020539288e57ebcdb0fd7669eabe21eb1ae776b0
Mitch Sullivan <sullivan.mitch@gmail.com>
2021-02-07 13:13:47 -0330

    Move away from indicator-specific dataset classes toward indicator-specific computation classes

--- src/indicators.py
+++ src/indicators.py
@@ -1,34 +1,46 @@
 """
 Tools for computing technical indicators of stock price data.
 """
-from pathlib import Path
+from typing import Optional
 
-import pandas as pd
-
+from src import logger
 from src.stock_price_data import StockPriceDataset
 
 
-class IndicatorDataset(StockPriceDataset):
+class Indicator(object):
     """
-    Base class for a stock price dataset containing technical indicator data computed from the stock price data.
+    Abstract base class for computing technical indicators.
     """
 
-    def __init__(self, stock_price_dataset: StockPriceDataset) -> None:
+    def __init__(self, field: str) -> None:
         """
-        Initialize an IndicatorDataset object from an existing StockPriceDataset object.
+        Initialize an Indicator object with the field of interest.
         """
-        super().__init__(stock_price_dataset.symbol, dataframe=stock_price_dataset.dataframe)
+        self.field = field
+        logger.debug(f"{self.__class__.__name__} field = {self.field}")
 
-    def compute(self) -> None:
+    def is_valid(self, dataset: StockPriceDataset) -> bool:
         """
-        Pure abstract method for computing the 
+        Ensure the dataset is valid. For now, this entails ensuring that the dataset contains the field of interest.
         """
-        raise NotImplementedError(f"Cannot compute technical indicator data for abstract base class {self.__class__.__name__}")
+        if self.field not in dataset.dataframe:
+            logger.error(f"Dataset does not contain field '{self.field}'")
+            return False
+        return True
+
+    def __call__(self, dataset: StockPriceDataset) -> StockPriceDataset:
+        """
+        Pure abstract method. Subclasses should implement this method with the logic specific to the technical
+        indicator.
+        """
+        raise NotImplementedError(
+            f"Cannot compute technical indicator data for abstract base class {self.__class__.__name__}"
+        )
 
 
-class MACDDataset(IndicatorDataset):
+class MACD(Indicator):
     """
-    Indicator dataset containing MACD data.
+    MACD indicator computation class.
     """
 
     FAST_EWMA: str = "Fast EWMA"
@@ -37,34 +49,29 @@ class MACDDataset(IndicatorDataset):
     MACD_SIGNAL: str = "MACD Signal"
     MACD_HISTOGRAM: str = "MACD Histogram"
 
-    def __init__(self,
-        stock_price_dataset: StockPriceDataset,
-        field: str="Close",
-        fast_ewma_span: int=12,
-        slow_ewma_span: int=26,
-        signal_span: int=9
-    ) -> None:
+    def __init__(self, field: str="Close", fast_ewma_span: int=12, slow_ewma_span: int=26, signal_span: int=9) -> None:
         """
-        Initialize a MACDDataset object from a StockPriceDataset, specifying the field defining the timeseries data,
-        the fast and slow EWMA spans, and the signal span.
+        Initialize a MACD object, specifying the field defining the timeseries data, the fast and slow EWMA spans, and
+        the signal span.
         """
-        super().__init__(stock_price_dataset)
-        if field not in self.dataframe:
-            raise KeyError(f"Dataset does not contain field '{field}'")
-        self.field: str = field
         if slow_ewma_span <= fast_ewma_span:
             raise ValueError(
                 f"Span {fast_ewma_span} of fast EWMA is greater than or equal to span {slow_ewma_span} of slow EWMA"
             )
+        super().__init__(field)
         self.fast_ewma_span: int = fast_ewma_span
         self.slow_ewma_span: int = slow_ewma_span
         self.signal_span: int = signal_span
-        self._computed: bool = False
+        logger.debug(f"{self.__class__.__name__} fast_ewma_span = {self.fast_ewma_span}")
+        logger.debug(f"{self.__class__.__name__} slow_ewma_span = {self.slow_ewma_span}")
+        logger.debug(f"{self.__class__.__name__} signal_span = {self.signal_span}")
 
-    def compute(self) -> None:
+    def __call__(self, dataset: StockPriceDataset) -> Optional[StockPriceDataset]:
         """
-        Compute the MACD indicator of the timeseries data of the specified field, assuming this hasn't yet been done.
-        If it has already been done, do nothing.
+        Ensure that the dataset is valid and, if it is, compute the MACD indicator of the timeseries data of the
+        specified field, save the data as new series in the dataset, and return the updated dataset.
+
+        If the dataset is not valid, return None.
 
         References:
 
@@ -72,10 +79,11 @@ class MACDDataset(IndicatorDataset):
             - https://en.wikipedia.org/wiki/MACD
             - https://www.investopedia.com/terms/m/macd.asp
         """
-        if not self._computed:
-            self.dataframe[self.FAST_EWMA] = self.dataframe[self.field].ewm(span=self.fast_ewma_span).mean()
-            self.dataframe[self.SLOW_EWMA] = self.dataframe[self.field].ewm(span=self.slow_ewma_span).mean()
-            self.dataframe[self.MACD_LINE] = self.dataframe[self.FAST_EWMA] - self.dataframe[self.SLOW_EWMA]
-            self.dataframe[self.MACD_SIGNAL] = self.dataframe[self.MACD_LINE].ewm(span=self.signal_span).mean()
-            self.dataframe[self.MACD_HISTOGRAM] = self.dataframe[self.MACD_LINE] - self.dataframe[self.MACD_SIGNAL]
-            self._computed = True
+        if not self.is_valid(dataset):
+            return None
+        dataset.dataframe[self.FAST_EWMA] = dataset.dataframe[self.field].ewm(span=self.fast_ewma_span).mean()
+        dataset.dataframe[self.SLOW_EWMA] = dataset.dataframe[self.field].ewm(span=self.slow_ewma_span).mean()
+        dataset.dataframe[self.MACD_LINE] = dataset.dataframe[self.FAST_EWMA] - dataset.dataframe[self.SLOW_EWMA]
+        dataset.dataframe[self.MACD_SIGNAL] = dataset.dataframe[self.MACD_LINE].ewm(span=self.signal_span).mean()
+        dataset.dataframe[self.MACD_HISTOGRAM] = dataset.dataframe[self.MACD_LINE] - dataset.dataframe[self.MACD_SIGNAL]
+        return dataset
